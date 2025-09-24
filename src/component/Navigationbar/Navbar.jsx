57ad5064edd65ded1af51327e2e201ebabfa7f20
Heshan386 <lakshithaheshan421@gmail.com>
2025-09-24 16:54:37 +0530

    changes in home page🏠

--- src/component/Navigationbar/Navbar.jsx
+++ src/component/Navigationbar/Navbar.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 const Navbar = () => {
+
+const [nav,setNav]= useState("");
+
   return (
     <div className='mainNav'>
         <h1>Yummy.</h1>
-        <div className="navList">
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Event</li>
-                <li>Chefs</li>
-                <li>Gallery</li>
-                <li>Contact</li>
+                <li onClick={()=>setNav("home")} className={nav==="home"?"active":""}>Home</li>
+                <li onClick={()=>setNav("about")} className={nav==="about"?"active":""}>About</li>
+                <li onClick={()=>setNav("menu")} className={nav==="menu"?"active":""}>Menu</li>
+                <li onClick={()=>setNav("event")} className={nav==="event"?"active":""}>Event</li>
+                <li onClick={()=>setNav("chefs")} className={nav==="chefs"?"active":""}>Chefs</li>
+                <li onClick={()=>setNav("gallery")} className={nav==="gallery"?"active":""}>Gallery</li>
+                <li onClick={()=>setNav("contact")} className={nav==="contact"?"active":""}>Contact</li>
             </ul>
-        </div>
         <button>Book a Table</button>
     </div>
   )
